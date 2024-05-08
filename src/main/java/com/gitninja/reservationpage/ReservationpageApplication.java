e6d303a17f7c6089885d00c884c485b8c88381d4
Juan Munguia <carlosmunguia23145@gmail.com>
2024-05-08 21:02:49 +0200

    Create new proyect backend

---
+++ src/main/java/com/gitninja/reservationpage/ReservationpageApplication.java
@@ -0,0 +1,13 @@
+package com.gitninja.reservationpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationpageApplication.class, args);
+	}
+
+}
